c5c6dac0e5f80f74cfb094c64b3f0a9a4109e8b7
minami <tantatatanaya@yahoo.co.jp>
2019-06-30 10:39:13 +0900

    add image

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,6 @@
 class Micropost < ApplicationRecord
+mount_uploader :image, ImageUploader
+
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
